063f4bdde25bdc9df2846d0cb9cba2b263f1dfc1
Reconion <luikrist@gmail.com>
2018-11-27 22:30:13 +0200

    merge

--- javascript/akordion.js
+++ javascript/akordion.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
             $(".title").click(function() {
                 console.log('click');
                 $(this).nextAll('p').slideToggle();
+                return false;
             });
         });
