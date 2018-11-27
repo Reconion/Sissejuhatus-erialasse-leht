93ee4d3ed457c5a5944bc46dca40d0361cf203a4
ingridraastas <ingridraastas@hot.ee>
2018-11-27 20:10:41 +0200

    akordion

--- javascript/akordion.js
+++ javascript/akordion.js
@@ -1,14 +1,6 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
+$(document).ready(function() {
+            $(".title").click(function() {
+                console.log('click');
+                $(this).nextAll('ol').slideToggle();
+            });
+        });
